e6b389ccac58efdc38f40bd84e055a0f4cc5bdfc
Andres Cruz <andres.cruz@utexas.edu>
2023-08-07 10:18:32 -0500

    Generate new setup materials

--- script_setup.R
+++ script_setup.R
@@ -1,4 +1,4 @@
-# Process QOG data for Ch. 4
+# Process QOG data for Ch. 4 ----
 
 library(tidyverse)
 library(haven)
@@ -20,6 +20,12 @@ qog <- qog_raw |>
            6 ~ "French",
            7 ~ "Portuguese"
          )) |> 
+  mutate(cname = case_when(
+    cname == "Bolivia (Plurinational State of)" ~ "Bolivia",
+    cname == "Venezuela (Bolivarian Republic of)" ~ "Venezuela",
+    cname == "United States of America (the)" ~ "United States",
+    TRUE ~ cname
+  )) |> 
   select(cname, ccodealp, year, region, wdi_pop, vdem_polyarchy, vdem_corr,
          ht_colonial)
   
@@ -29,7 +35,7 @@ write_dta(qog, "data/sample_qog_bas_ts_jan23.dta")
 write_sav(qog, "data/sample_qog_bas_ts_jan23.sav")
 write_xlsx(qog, "data/sample_qog_bas_ts_jan23.xlsx")
 
-# Generate vertical line test figure
+# Generate vertical line test figure ----
 
 library(patchwork)
 
@@ -90,3 +96,98 @@ p_patchw
 ggsave(plot = p_patchw, filename = "images/vertical_line_test.png",
        scale = .85, width = 8, height = 5)
 
+# Generate class materials ----
+
+library(stringr)
+
+# identify and read Quarto files
+
+files_part1 <- c("01_r_intro.qmd", "02_tidy_data1.qmd", "03_matrices.qmd",
+                 "04_tidy_data2.qmd", "05_functions.qmd")
+
+l_lines_part1 <- lapply(files_part1, readLines)
+
+# extract titles
+
+titles_part1 <- sapply(
+  l_lines_part1, function(x){x |> str_subset("^\\#\\s")} |> head(1)
+) |> 
+  str_remove("^#\\s")
+  
+# add header
+
+f_add_top <- function(body, title){
+  to_add <- c("---", 
+              sprintf("title: 'Methods Camp - %s'", title),
+              "date: 'Summer 2023'", 
+              "editor: visual",
+              "---", 
+              "")
+  print(to_add)
+  c(to_add, body)
+}
+
+l_lines_part1_edit <- lapply(seq_along(files_part1), function(i){
+  f_add_top(body = l_lines_part1[[i]], title = titles_part1[[i]])
+})
+
+# save Quarto files
+
+for (i in seq_along(l_lines_part1_edit)){
+  writeLines(l_lines_part1_edit[[i]], 
+             str_c("materials/methodscamp_part1/", files_part1[[i]]))
+}
+
+# copy images
+
+imgs_part1 <- lapply(l_lines_part1, str_subset, "images/") |> 
+  unlist() |> 
+  str_extract("images/.*((jpg)|(png))") |> 
+  na.omit()
+
+imgs_folder <- "materials/methodscamp_part1/images"
+dir.create(imgs_folder)
+for (i in seq_along(imgs_part1)){
+  file.copy(imgs_part1[[i]], imgs_folder)
+}
+
+# copy data files
+
+data_part1 <- lapply(l_lines_part1, str_subset, "data/") |> 
+  unlist() |> 
+  str_extract("data/\\S*((rds)|(csv)|(xlsx)|(dta)|(sav)|(xls))") |> 
+  na.omit()
+
+data_folder <- "materials/methodscamp_part1/data"
+dir.create(data_folder)
+for (i in seq_along(data_part1)){
+  file.copy(data_part1[[i]], data_folder)
+}
+
+# create .Rproj
+
+text_rproj <- c("Version: 1.0", 
+                "", 
+                "RestoreWorkspace: Default",
+                "SaveWorkspace: Default",
+                "AlwaysSaveHistory: Default",
+                "",
+                "EnableCodeIndexing: Yes",
+                "UseSpacesForTab: Yes",
+                "NumSpacesForTab: 2",
+                "Encoding: UTF-8",
+                "",
+                "RnwWeave: Sweave", 
+                "LaTeX: pdfLaTeX")
+
+writeLines(text_rproj, "materials/methodscamp_part1/methodscamp_part1.Rproj")
+
+# create .zip file
+
+setwd("materials")
+zip(zipfile = "methodscamp_part1.zip", 
+    files = "methodscamp_part1/")
+
+unlink("methodscamp_part1/", recursive = T)
+
+setwd("..")
